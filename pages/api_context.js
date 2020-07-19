4a780436cdeb2768665c1e8cb18fff1fda3d3abe
kimyongyeon <toyongyeon@gmail.com>
2020-07-19 17:12:38 +0900

    React.memo를 사용한 최적화

--- pages/api_context.js
+++ pages/api_context.js
@@ -1,25 +1,37 @@
+// React.createContext 함수구조
+// React.createContext(defaultValue) => {Provider, Consumer}
 const UserContext = React.createContext('');
 
 // Context API로 데이터 전달하기 
 function App() {
+    const [username, setUsername] = useState('');
+    /**
+     * Provider 컴포넌트의 속성값이 변경되면, 하위의 모든 consumer 컴포넌트는 다시 렌더링 된다. 
+     * 한가지 중요한점, 중간에 위치한 컴포넌트의 랜더링 여부와 상관없이 consumer 컴포넌트는 다시 랜더링된다는 점
+     */
     return (
         <div>
-            <UserContext.Provider value="mike">
-                <div>상단메뉴</div>
+
+            <UserContext.Provider value={username}>
                 <Profile />
-                <div>하단메뉴</div>
             </UserContext.Provider>
+            <input
+                type="text"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+            />
         </div>
     );
 }
 
-function Profile() {
+const Profile = React.memo(() => {
     return (
         <div>
             <Greeting />
         </div>
     );
-}
+});
+    
 
 function Greeting() {
     return (
