3e2167cc491cb3886d4d8215cca2f30c85115d1a
kimyongyeon <toyongyeon@gmail.com>
2020-07-20 22:24:33 +0900

    ref 속성값으로 함수 사용하기 고정된 함수 사용전

--- pages/ref.js
+++ pages/ref.js
@@ -3,7 +3,7 @@
 // 2. ref 속성값 활용하기 : forwardRef 
 // ref.current : 해당 클래스의 메서드를 호출할 수 있다. 
 // useImpreativeHandle : 함수형 컴포넌트에서도 변수와 함수를 외부로 노출시킬 수 있다.
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 
 // forwardRef 함수로 ref 속성값을 직접 처리하기
 const TextInput = React.forwardRef((props, ref) => ( // forwardRef 함수를 이용하면 부모컴포넌트에서 넘어온 ref 속성값을 직접 처리할 수 있다.
@@ -13,16 +13,22 @@ const TextInput = React.forwardRef((props, ref) => ( // forwardRef 함수를 이
     </div>
 ));
 
+// ref 속성값으로 함수 사용하기 
 function Form() {
-    const inputRef = useRef();
-    useEffect(() => {
-        inputRef.current.focus();
-    }, []);
+    const INITIAL_TEXT = "안녕하세요.";
+    const [text, setText] = useState(INITIAL_TEXT);
+    const [showText, setShowText] = useState(true);
     return (
         <div>
-            {/* 이전 코드에서 inputRef로 사용했던 이름을 리액트의 예약어인 ref로 사용할 수 있게 됐다. */}
-            <TextInput ref={inputRef} />
-            <button onClick={() => inputRef.current.focus()}>텍스트로 이동</button>
+            {showText && (
+                <input
+                    type="text"
+                    ref={ref => ref && setText(INITIAL_TEXT)}
+                    value={text}
+                    onChange={e => setText(e.target.value)}
+                />
+            )}
+            <button onClick={() => setShowText(!showText)}>보이기/가리기</button>
         </div>
     )
 }
